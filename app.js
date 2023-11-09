8b7b372533db753ef1c14e5cf01cd22597ffdd5a
thanh <miktenthanh@gmail.com>
2023-11-10 02:07:29 +0700

    update

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-// const btnNext = document.querySelector(".btn-next");
-// const btnPrevious = document.querySelector(".btn-previous");
-// btnNext.addEventListener("click", () => Next());
-// btnNext.addEventListener("click", () => Previous());
+const barIcon = document.querySelector(".bar-icon");
+const menu = document.querySelector("#menu");
+const navBar = document.querySelector(".nav-bar");
+barIcon.addEventListener("click", function (e) {
+  menu.classList.toggle("active");
+});
+const handeScroll = () => {
+  if (window.scrollY > 700) {
+    navBar.classList.add("navbar-active");
+  } else {
+    navBar.classList.remove("navbar-active");
+  }
+};
+window.addEventListener("scroll", handeScroll);
 
 let img = [];
 for (let i = 1; i < 4; i++) {
   img[i] = new Image();
   img[i].src = "/imgs/banner" + i + ".jpg";
 }
-console.log(img[1].src);
-var index = 0;
+let index = 0;
 function auto() {
   index++;
-  if (index == img.length) index = 0;
-  var anh = document.getElementById("img-banner");
-  anh.src = img[index].src;
+  if (index == img.length) index = 1;
+  let anh = document.getElementById("img-banner");
+  anh.src = img[index]?.src;
   t = setTimeout("auto()", 5000);
 }
 const Previous = () => {
   index--;
   if (index < 0) index = img.length;
-  var anh = document.getElementById("img-banner");
+  let anh = document.getElementById("img-banner");
   anh.src = img[index].src;
 };
 const Next = () => {
   index++;
-  if (index >= img.length) index = 0;
-  var anh = document.getElementById("img-banner");
-  anh.src = img[index].src;
+  if (index >= img.length) index = 1;
+  let anh = document.getElementById("img-banner");
+  anh.src = img[index]?.src;
 };
-window.onload(auto());
+
+window.addEventListener("load", function () {
+  auto();
+});
